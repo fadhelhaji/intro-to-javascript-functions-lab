e0ad6dd776bcf9c4e68dbafb39bb49955ae44c66
fadhelhaji <fadhelmh@hotmail.com>
2025-11-11 14:02:14 +0300

    Added comments indicating each exercise number

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Exercise 1
 function maxOfTwoNumbers(higher,lower){
     if(higher >= lower){
         return higher
@@ -8,6 +9,8 @@ function maxOfTwoNumbers(higher,lower){
 }
 console.log(maxOfTwoNumbers(10,9));
 
+
+//Exercise 2
 const isAdult = age => {
     if(age >= 18){
         return 'adult'
@@ -18,6 +21,8 @@ const isAdult = age => {
 }
 console.log(isAdult(18))
 
+
+//Exercise 3
 const isCharVowel = vowels => {
     if( vowels === 'a'||
         vowels === 'e'||
@@ -32,11 +37,15 @@ const isCharVowel = vowels => {
 }
 console.log(isCharVowel('o'))
 
+
+//Exercise 4
 const generateEmail = (name, domain) =>{
     return (name + domain)
 }
 console.log(generateEmail('fadhel','@hotmail.com'))
 
+
+//Exercise 5
 const greetUser = (time, name) =>{
     if(time === 'morning'){
         return 'Good morning! ' + name
@@ -50,6 +59,8 @@ const greetUser = (time, name) =>{
 }
 console.log(greetUser('morning', 'Fadhel'))
 
+
+//Exercise 6
 const maxOfThree = (one, two, three) => {
     {
         return Math.max(one , two, three)
@@ -57,11 +68,15 @@ const maxOfThree = (one, two, three) => {
 }
 console.log(maxOfThree('10', '6', '8'))
 
+
+//Exercise 7
 const calculateTip = (totalBill, tipPercentage) => {
     return (totalBill/100) * tipPercentage
 }
 console.log(calculateTip(50, 20))
 
+
+//Exercise 8
 const convertTemp = (Temp, Scale)=>{
     if(Scale === 'C'){
         return (Temp*9/5)+32
@@ -72,6 +87,8 @@ const convertTemp = (Temp, Scale)=>{
 }
 console.log(convertTemp(32, 'F'))
 
+
+//Exercise 9
 const basicCalculator = (num1, num2, operation) => {
     if(operation === 'subtract'){
         return(num1 - num2)
